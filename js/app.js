7001e2a8a6ab609edbbe963ce1eeb4df6b478519
Xiande Qiu <Xiandeqiu@gmail.com>
2020-12-16 12:28:36 +0100

    hola

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 // Tu código aquí
+const button = document.querySelector('#btn')
+
+button.addEventListener('click', changeHex)
+
+function changeHex() {
+
+    var hex = Math.floor(Math.random() * 16777215).toString(16);
+
+    document.body.style.backgroundColor = "#" + hex
+
+}
